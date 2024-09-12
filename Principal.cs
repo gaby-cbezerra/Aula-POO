1f4d05f16dd31de2e56a260673ead855bf511976
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-09-12 16:29:45 -0300

    Conta bancária
    Código da conta bancária
    Aula: 12/09/24

---
+++ Principal.cs
@@ -0,0 +1,8 @@
+using System;
+class Principal
+{
+    static void Main(string[] args)
+    {
+        Conta minhaConta = new Conta("Maria", "Inter", 12, 87491094, 20.00);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Aula_POO;
+
+class Program{
+
+    public class Conta
+    {
+        public string titular;
+
+        public string banco;
+
+        public int numeracao;
+
+        public int cpf;
+
+        public int saldo;
+
+        public Conta(string nomeTitular, string nomeBanco, int numberNumeracao, int numberCpf, int numberSaldo)
+        {
+            titular = nomeTitular;
+            banco = nomeBanco;
+            numeracao = numberNumeracao;
+            cpf = numberCpf;
+            saldo = numberSaldo;
+        }
+    }
+}
